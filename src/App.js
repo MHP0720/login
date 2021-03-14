1641b402aa964f9c6a1327b08d0bc66f5c5cb864
MHP0720 <dmmehediparash@gmail.com>
2021-03-14 19:58:19 +0700

    login

--- src/App.js
+++ src/App.js
@@ -1,23 +1,129 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+firebase.initializeApp(firebaseConfig);
 
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    password: '',
+    photo: '',
+    error:''
+  });
+
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then(res => {
+        const { displayName, photoURL, email } = res.user;
+        const signedInUser = {
+          isSignedIn: true,
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(signedInUser);
+      })
+      .catch(err => {
+        console.log(err);
+        console.log(err.message);
+      })
+  }
+
+  const handleSignOut = () => {
+    firebase.auth().signOut()
+      .then(res => {
+        const signedOutUser = {
+          isSignedIn: false,
+          name: '',
+          email: '',
+          photo: '',
+          error: ''
+        }
+        setUser(signedOutUser);
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  }
+
+ 
+  const handleBlur = (event) => {
+    let isFieldValid = true;
+    // console.log(event.target.name, event.target.value);
+    if (event.target.name === 'email') {
+      isFieldValid = /\S+@\S+\.\S+/.test(event.target.value);
+    }
+    if (event.target.name === 'password') {
+      const isPasswordValid = event.target.value > 6;
+      const passwordHasNumber = /\d{1}/;
+      isFieldValid = isPasswordValid && passwordHasNumber;
+    }
+    if (isFieldValid) {
+      const newUserInfo = { ...user };
+      newUserInfo[event.target.name] = event.target.value;
+      setUser(newUserInfo);
+    }
+  }
+
+
+  const handleSubmit = (e) => {
+    if (user.email && user.password){
+      firebase.auth().createUserWithEmailAndPassword(user.email, user.password)
+      .then(res=>{
+        console.log(res);
+        const newUserInfo = {...user};
+        newUserInfo.error = '';
+        setUser(newUserInfo);
+      })
+       
+         .catch(error => {
+          const newUserInfo = {...user};
+          newUserInfo.error =error.message;
+          setUser(newUserInfo);
+
+        //   // ..
+         });
+    }
+    e.preventDefault()
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.isSignedIn ? <button onClick={handleSignOut}>Sign out</button> :
+          <button onClick={handleSignIn}>Sign in</button>
+      }
+
+      {
+        user.isSignedIn && <div>
+          <p>Welcome, {user.name}</p>
+          <p>Your email: {user.email}</p>
+          <img src={user.photo} alt="" />
+        </div>
+      }
+      <h1>Our Own Authentication</h1>
+
+      <form onSubmit={handleSubmit}>
+        <input name="name" onBlur={handleBlur} type="text" placeholder="Your name" />
+        <br />
+        <input type="text" name="email" onBlur={handleBlur} placeholder="Your Email Address" required />
+        <br />
+        <input type="password" name="password" onBlur={handleBlur} placeholder="Your Password" required />
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+
+      <h1 style={{color:'red'}}>{user.error}</h1>
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCLHVqZlx1wkHR4_Ce20j504a_KgxDFkFo",
+  authDomain: "ema-john-simple-mhp.firebaseapp.com",
+  projectId: "ema-john-simple-mhp",
+  storageBucket: "ema-john-simple-mhp.appspot.com",
+  messagingSenderId: "967527131548",
+  appId: "1:967527131548:web:37cef4f15938a30d974059",
+  measurementId: "G-2R3RX1GJZE"
+};
+
+export default firebaseConfig;
